72f20e8b30c94e325fe5fa83a8cc1db0077532b5
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-11 13:26:59 +0300

    feat(core): add core module and utils package
    this commit adds essential utility functions used to set up django-split-settings

---
+++ routine_tracker/core/utils/misc.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+from typing import Any, Union
+
+
+def yaml_coerce(value: Union[str, Any]) -> Any:
+    """
+    Coerce a value to a Python object using PyYAML. This function is useful for
+    converting environment variables to Python objects.
+    """
+
+    # Convert value to a Python object
+    if isinstance(value, str):
+        return yaml.safe_load(f'dummy: {value}')['dummy']
+
+    # If the value is not a string, return it as is
+    return value
+
+
+def get_env(name: str, default: Any = None) -> Any:
+    """
+    Get an environment variable with the given name. If the environment variable
+    is not set, return the default value.
+    """
+
+    return yaml_coerce(default) if (value := os.environ.get(name)) is None else yaml_coerce(value)
