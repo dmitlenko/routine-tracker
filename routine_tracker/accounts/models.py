433dedb67dfff0026c739508afdb76aaa10276b2
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-13 21:00:52 +0300

    feat: add UserProfile model to store user preferences

--- routine_tracker/accounts/models.py
+++ routine_tracker/accounts/models.py
@@ -14,3 +14,30 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class UserProfile(models.Model):
+    """
+    User profile model with additional user preferences
+    """
+
+    class Language(models.TextChoices):
+        """
+        Language choices for the user profile
+        """
+
+        ENGLISH = 'en', 'English'
+        UKRAINIAN = 'uk', 'Ukrainian'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    dark_mode = models.BooleanField(default=None, null=True)
+    preferred_language = models.CharField(
+        max_length=2,
+        choices=Language.choices,
+        default=Language.ENGLISH,
+    )
+    time_zone = models.CharField(max_length=100, default='UTC')
+    profile_completeness = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
