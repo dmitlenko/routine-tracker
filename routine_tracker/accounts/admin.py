1aac70c66ee802ba9962f72106f74209afca98b5
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-14 00:17:39 +0300

    feat: update User model and add UserManager
    This commit removes `username` field from User model, adds two required fields: `first_name` and `last_name`. Sets email as username field and updates __str__ method.

--- routine_tracker/accounts/admin.py
+++ routine_tracker/accounts/admin.py
@@ -2,18 +2,49 @@ from typing import Any
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from routine_tracker.accounts.models import User, UserProfile
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("password",)}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("usable_password", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("email",)
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'dark_mode', 'preferred_language', 'time_zone')
     list_filter = ('dark_mode', 'preferred_language', 'time_zone')
-    search_fields = ('user__username', 'user__email')
-    ordering = ('user__username',)
+    search_fields = ('user__email', 'user__first_name', 'user__last_name')
     raw_id_fields = ('user',)
     list_select_related = ('user',)
     fieldsets = (
--- routine_tracker/accounts/models.py
+++ routine_tracker/accounts/models.py
@@ -1,6 +1,34 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -8,13 +36,18 @@ class User(AbstractUser):
     Custom user model with email as the unique identifier
     """
 
-    email = models.EmailField(unique=True)
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
 
-    REQUIRED_FIELDS = ['email']
-    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
 
 
 class UserProfile(models.Model):
@@ -42,7 +75,7 @@ class UserProfile(models.Model):
     profile_completeness = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
-        return _("{username}'s profile").format(username=self.user.username)
+        return _("{username} profile").format(username=self.user.email)
 
     class Meta:
         verbose_name = _('User profile')
