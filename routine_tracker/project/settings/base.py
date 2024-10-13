eaa6c3573336c8d756c3ca9d488f907f74645fc0
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-13 21:41:33 +0300

    feat: register core package as django app

--- routine_tracker/project/settings/base.py
+++ routine_tracker/project/settings/base.py
@@ -9,6 +9,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'routine_tracker.core',
     'routine_tracker.accounts',
 ]
 
