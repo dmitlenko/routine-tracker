018dbf6d017a3e58dd247b06d60899e269e0a7c6
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-13 19:43:30 +0300

    refactor: update core module test to use unittest

--- routine_tracker/core/tests/test_collections.py
+++ routine_tracker/core/tests/test_collections.py
@@ -1,24 +1,34 @@
+from django.test import TestCase
+
 from ..utils.collections import deep_update
 
 
-def test_deep_update():
-    # Define the base dictionary
-    base_dict = {
-        "a": 1,
-        "b": {"c": 2, "d": 3},
-    }
+class CollectionsTestCase(TestCase):
+    def test_deep_update(self):
+        # Define the base dictionary
+        base_dict = {
+            "a": 1,
+            "b": {"c": 2, "d": 3},
+        }
 
-    # Define the update_with dictionary
-    update_with = {
-        "a": 4,
-        "b": {"c": 5},
-    }
+        # Define the update_with dictionary
+        update_with = {
+            "a": 4,
+            "b": {"c": 5},
+        }
 
-    # Update the base dictionary with the update_with dictionary
-    updated_dict = deep_update(base_dict, update_with)
+        # Update the base dictionary with the update_with dictionary
+        updated_dict = deep_update(base_dict, update_with)
 
-    # Check that the base dictionary was updated correctly
-    assert updated_dict == {
-        "a": 4,
-        "b": {"c": 5, "d": 3},
-    }, "deep_update should update the base dictionary correctly"
+        # Define the expected dictionary
+        expected_dict = {
+            "a": 4,
+            "b": {"c": 5, "d": 3},
+        }
+
+        # Check that the base dictionary was updated correctly
+        self.assertEqual(
+            updated_dict,
+            expected_dict,
+            "deep_update should update the base dictionary correctly",
+        )
