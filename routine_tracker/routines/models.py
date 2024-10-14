8792b6373fc91b1ec73ca69867f13b234f49dc99
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-14 14:54:45 +0300

    refactor: Add `statistics` method to Routine model
    Introduces a `statistics` method for calculating general and streak-based metrics. Adds typed dictionaries for structuring results and private helpers for calculations, enhancing routine tracking.

--- routine_tracker/routines/models.py
+++ routine_tracker/routines/models.py
@@ -1,8 +1,36 @@
+from datetime import date, timedelta
+from typing import Dict, Tuple, TypedDict, Union
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
+
+
+StreakStatistics = TypedDict(
+    'StreakStatistics',
+    {
+        'completeness': float,
+        'days': int,
+        'start': date,
+        'end': date,
+        'missed': int,
+    },
+)
+
+
+RoutineStatistics = TypedDict(
+    'RoutineStatistics',
+    {
+        'total': int,
+        'completed': int,
+        'average': float,
+        'best': int,
+        'worst': int,
+        'streak': StreakStatistics,
+    },
+)
 
 
 class RoutineGroup(models.Model):
@@ -38,6 +66,83 @@ class Routine(models.Model):
     has_goal = models.BooleanField(default=False)
     goal = models.PositiveIntegerField(blank=True, null=True)
     measure = models.CharField(max_length=50, blank=True, choices=Measure.choices, default=Measure.SECONDS)
+
+    def _calculate_general_statistics(self, entries: 'models.QuerySet[RoutineEntry]') -> Dict[str, Union[int, float]]:
+        total = len(entries)
+        completed = len([entry for entry in entries if entry.value >= self.goal])
+        average = sum([entry.value for entry in entries]) / total
+        best = max([entry.value for entry in entries])
+        worst = min([entry.value for entry in entries])
+
+        return {
+            'total': total,
+            'completed': completed,
+            'average': average,
+            'best': best,
+            'worst': worst,
+        }
+
+    def _calculate_streak(self, entries: 'models.QuerySet[RoutineEntry]', start: date) -> StreakStatistics:
+        # Ensure entries are sorted by date
+        dates = [entry.date for entry in entries]
+
+        total = len(entries)
+        end_date = date.today()
+        missed = (end_date - start).days - total
+        streak_days = 0
+
+        current_date = end_date - timedelta(days=1)  # Exclude today
+
+        for entry_date in dates:
+            if entry_date == current_date:
+                streak_days += 1
+                current_date -= timedelta(days=1)
+            else:
+                break
+
+        return {
+            'missed': missed,
+            'completeness': streak_days / total,
+            'days': streak_days,
+            'start': current_date,
+            'end': end_date,
+        }
+
+    def statistics(
+        self, start: date, end: date = None
+    ) -> Union[Tuple[RoutineStatistics, 'models.QuerySet[Routine]'], None]:
+        """Calculate statistics for this routine within a given date range.
+
+        Args:
+            start (date): Start date for the statistics.
+            end (date, optional): End date for the stastics. Defaults to None.
+
+        Returns:
+            Union[Tuple[RoutineStatistics, QuerySet[Routine]], None]:
+            Tuple containing the statistics and the entries for the routine.
+        """
+
+        # Get all entries for this routine
+        # If an end date is not provided, return all entries after the start date
+        query = self.entries.order_by('-date')
+
+        if end is not None:
+            entries = query.filter(date__range=[start, end])
+        else:
+            entries = query.filter(date__gte=start)
+
+        # If there are no entries, return None
+        if not entries:
+            return None
+
+        # Calculate general statistics
+        general_stats = self._calculate_general_statistics(entries)
+
+        # Calculate streak
+        streak = self._calculate_streak(entries, start)
+
+        # Return statistics
+        return {**general_stats, 'streak': streak}, entries
 
     def __str__(self):
         return self.name
