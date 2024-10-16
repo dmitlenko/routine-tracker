e3ad40a5b957fe719bba4eb9206e8bfc9aa60a32
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-16 22:35:56 +0300

    refactor: update navbar component to use LinkHolder class for rendering user menu links

--- routine_tracker/base/components/navbar/navbar.py
+++ routine_tracker/base/components/navbar/navbar.py
@@ -1,35 +1,21 @@
-from dataclasses import dataclass
-from typing import Any, Literal, TypedDict, Union, Unpack
+from typing import Any
 
 from django.http import HttpRequest
-from django.urls import reverse
 from django_components import Component, register
 
+from routine_tracker.base.utils.nav import lks
 
-@dataclass
-class NavbarLink:
-    reverse_name: str
-    text: str
-
-    url: str = '#'
-    active: Union[Literal[''], Literal["active"]] = None
-
-
-class Kwargs(TypedDict):
-    request: HttpRequest
-
-
-NAVLINKS = [
-    NavbarLink('base:index', 'Home'),
-    NavbarLink('routines:routine-group-list', 'Routines'),
-]
+links = lks(
+    ('base:index', 'Home'),
+    ('routines:routine-group-list', 'Routines'),
+)
 
 
 @register('navbar')
 class NavbarComponent(Component):
     template_name = 'navbar.html'
 
-    def get_context_data(self, *args: Any, **kwargs: Unpack[Kwargs]) -> Any:
+    def get_context_data(self, *args: Any, **kwargs: Any) -> Any:
         # Get the request object from the kwargs
         request: HttpRequest = self.inject('request').request
 
@@ -37,24 +23,7 @@ class NavbarComponent(Component):
         if not request.user.is_authenticated:
             return {'is_authenticated': False}
 
-        # Create a copy of the navlinks list
-        navlinks = NAVLINKS.copy()
-
-        # Loop through the navlinks list and set the active attribute
-        for link in navlinks:
-            # Check if the current view name matches the reverse_name of the link
-            is_active = request.resolver_match.view_name == link.reverse_name
-
-            # Set the active attribute to 'active' if the link is active
-            link.active = 'active' if is_active else ''
-
-            # Set the url of the link to the reverse_name if the link is not active
-            if is_active:
-                link.url = '#'
-            else:
-                link.url = reverse(link.reverse_name)
-
         return {
-            'navlinks': navlinks,
+            'navlinks': links.reverse(request),
             'is_authenticated': True,
         }
