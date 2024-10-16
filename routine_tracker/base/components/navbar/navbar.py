adef5609616413a5fdf6b5588ff7ab79559b3b45
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-16 17:53:55 +0300

    feat: add new navbar component to render predefined menu links

---
+++ routine_tracker/base/components/navbar/navbar.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from typing import Any, Literal, TypedDict, Union, Unpack
+
+from django.http import HttpRequest
+from django.urls import reverse
+from django_components import Component, register
+
+
+@dataclass
+class NavbarLink:
+    reverse_name: str
+    text: str
+
+    url: str = '#'
+    active: Union[Literal[''], Literal["active"]] = None
+
+
+class Kwargs(TypedDict):
+    request: HttpRequest
+
+
+NAVLINKS = [
+    NavbarLink('base:index', 'Home'),
+    NavbarLink('routines:routine-group-list', 'Routines'),
+]
+
+
+@register('navbar')
+class NavbarComponent(Component):
+    template_name = 'navbar.html'
+
+    def get_context_data(self, *args: Any, **kwargs: Unpack[Kwargs]) -> Any:
+        # Get the request object from the kwargs
+        request = kwargs['request']
+
+        # Create a copy of the navlinks list
+        navlinks = NAVLINKS.copy()
+
+        # Loop through the navlinks list and set the active attribute
+        for link in navlinks:
+            # Check if the current view name matches the reverse_name of the link
+            is_active = request.resolver_match.view_name == link.reverse_name
+
+            # Set the active attribute to 'active' if the link is active
+            link.active = 'active' if is_active else ''
+
+            # Set the url of the link to the reverse_name if the link is not active
+            if not is_active:
+                link.url = reverse(link.reverse_name)
+
+        return {
+            'navlinks': navlinks,
+        }
