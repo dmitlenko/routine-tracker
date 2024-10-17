253558c76c975de8f14ea9b5601aabdc8608fdc1
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-17 17:08:12 +0300

    feat: add FormComponent and template for form rendering

---
+++ routine_tracker/base/components/form/form.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+from django.forms import Form
+from django_components import Component, register
+from django_components.attributes import attributes_to_string
+
+
+@register('form')
+class FormComponent(Component):
+    template_name = 'template.html'
+
+    def get_context_data(self, form: Form, *, horizontal: str = '', boost: str = '', **kwargs) -> Dict:
+        return {
+            'form': form,
+            'horizontal': horizontal == 'true',
+            'boost': boost == 'true',
+            'form_attrs': attributes_to_string(kwargs),
+        }
