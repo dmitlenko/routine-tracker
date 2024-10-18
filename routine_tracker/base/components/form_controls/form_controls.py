534894124ea4749ea4d6166b7cb2c60e3672de2b
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-18 16:56:22 +0300

    refactr: update form controls template to use trimmed template tags for button classes and values

--- routine_tracker/base/components/form_controls/form_controls.py
+++ routine_tracker/base/components/form_controls/form_controls.py
@@ -12,16 +12,12 @@ class FormControlsComponent(Component):
         self,
         form_id: str = '',
         wrap: bool = False,
-        submit_text: str = "Submit",
-        dismiss_text: str = "Cancel",
         in_modal: bool = False,
         **kwargs,
     ) -> Any:
         return {
             'wrap': wrap,
             'form_id': attributes_to_string({'form': form_id}) if form_id else '',
-            'submit_text': submit_text,
-            'dismiss_text': dismiss_text,
             'in_modal': attributes_to_string({'data-bs-dismiss': 'modal'}) if in_modal else '',
             'attrs': attributes_to_string(kwargs) if kwargs else '',
         }
