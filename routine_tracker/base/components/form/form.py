a2f3179c6b512b1cf9f23dc4e42d5315ac090280
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-18 16:39:13 +0300

    refactor: update FormComponent to include attrs parameter in get_context_data

--- routine_tracker/base/components/form/form.py
+++ routine_tracker/base/components/form/form.py
@@ -9,11 +9,14 @@ from django_components.attributes import attributes_to_string
 class FormComponent(Component):
     template_name = 'template.html'
 
-    def get_context_data(self, form: Form, *, horizontal: str = '', boost: str = '', **kwargs) -> Dict:
+    def get_context_data(
+        self, form: Form, *, horizontal: str = '', boost: str = '', attrs: str = '', **kwargs
+    ) -> Dict:
         return {
             'form': form,
             'horizontal': horizontal == 'true',
             'boost': boost == 'true',
+            'attrs': attrs,
             'form_attrs': attributes_to_string(kwargs) if kwargs else '',
         }
 
