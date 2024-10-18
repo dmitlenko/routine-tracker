b6956d0cf718c191b03681b57d7cb4156485fe42
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-18 15:32:29 +0300

    feat: add FormControlsComponent for handling form controls

---
+++ routine_tracker/base/components/form_controls/form_controls.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+from django_components import Component, register
+from django_components.attributes import attributes_to_string
+
+
+@register('form-controls')
+class FormControlsComponent(Component):
+    template_name = 'template.html'
+
+    def get_context_data(
+        self,
+        form_id: str = '',
+        wrap: bool = False,
+        submit_text: str = "Submit",
+        dismiss_text: str = "Cancel",
+        in_modal: bool = False,
+        **kwargs,
+    ) -> Any:
+        return {
+            'wrap': wrap,
+            'form_id': attributes_to_string({'form': form_id}) if form_id else '',
+            'submit_text': submit_text,
+            'dismiss_text': dismiss_text,
+            'in_modal': in_modal,
+            'attrs': attributes_to_string(kwargs) if kwargs else '',
+        }
