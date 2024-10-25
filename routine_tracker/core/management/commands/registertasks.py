42e563bc7cf487fa994d678fb4cc6c7cb4e3cdb0
Denys Mitlenko <mitlenkodenis3@gmail.com>
2024-10-25 21:13:06 +0300

    feat: add register tasks command

---
+++ routine_tracker/core/management/commands/registertasks.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django_celery_beat.models import IntervalSchedule, PeriodicTask
+
+
+class Command(BaseCommand):
+    help = 'Compile locale files for the project'
+
+    tasks = [
+        (('routine_tracker.core.tasks.send_reminders', 'Send reminders'), (24, IntervalSchedule.HOURS)),
+    ]
+
+    def handle(self, *args, **options):
+        # Delete all periodic tasks
+        IntervalSchedule.objects.all().delete()
+        PeriodicTask.objects.all().delete()
+
+        for (task, name), (every, period) in self.tasks:
+            schedule = IntervalSchedule.objects.create(every=every, period=period)
+            PeriodicTask.objects.create(
+                interval=schedule,
+                name=name,
+                task=task,
+            )
+
+            self.stdout.write(self.style.SUCCESS(f'Created task: {name}'))
